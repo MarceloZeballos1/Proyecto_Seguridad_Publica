b56e238e5583b9851c4557b19c63d29bb638d1f2
MarceloZeballos1 <marcelo.zeballos@ucb.edu.bo>
2024-11-26 17:17:00 -0400

    Archivo db.php y imagenes

--- db.php
+++ db.php
@@ -7,6 +7,7 @@ $password = '';
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->exec("SET NAMES 'utf8'");
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
     exit();
